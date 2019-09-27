819e43f5075287f2bca9f5ce7ce1083ba0d58360
liuxkay <sccck000@gmail.com>
2019-09-27 09:14:52 +0900

    file updated

---
+++ dependencies.sh
@@ -0,0 +1,3 @@
+#just run this file sh depencencies.sh
+sudo apt -y install liblapack-dev libatlas-base-dev libjasper-dev libilmbase-dev libopenexr-dev libgstreamer1.0-dev libgstreamer1.0-dev libqt4-dev
+
